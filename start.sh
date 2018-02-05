62fb0867aa766bebce5454f9328064f8557ad0f6
Ubuntu <ubuntu@ip-172-31-40-168.us-east-2.compute.internal>
2018-02-05 18:01:02 +0000

    aws example

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-export FLASK_APP=hello.py
-flask run --host=0.0.0.0
+python app.py $*
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return ["<h1 style='color:blue'>Hello There!</h1>"]
+
